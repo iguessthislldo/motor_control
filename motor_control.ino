797b21a9729c3163dce4f55a27420ef6cbba5a55
Fred Hornsey <fred@hornsey.us>
2018-12-23 01:10:25 -0600

    Start to change into something closer to the desired behavior

--- motor_control.ino
+++ motor_control.ino
@@ -1,143 +1,164 @@
-#define LOG
+/*
+ * Serial Logging Control
+ * Should be disabled when this is uploaded for the last time
+ * 0: Disable
+ * 1: Enable
+ */
+#define LOG 1
+
+#if LOG == 1
+#define LOG_SPEED Serial.print("SPEED: "); Serial.print(speed); Serial.println('%');
+#else
+#define LOG_SPEED // Nothing
+#endif
+
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
 
 // Input =====================================================================
 
-// Switch 1
-#define SWITCH_1_PRESSED (digitalRead(5) == LOW)
-
-// Rotary Encoder
-const int RE_DT = 2;
-const int RE_CLK = 3;
-const int RE_INTERRUPT = 1; // Interrupt for Pin 3
+#define FORWARD_PRESSED (digitalRead(5) == LOW)
+#define FORWARD_DIR(value) (value)
+#define BACKWARD_PRESSED (digitalRead(6) == LOW)
+#define BACKWARD_DIR(value) -(value)
 
 // Output ====================================================================
 
-// Motor A
-const int MOTOR_A_DIR_1 = 13;
-const int MOTOR_A_DIR_2 = 12;
-const int MOTOR_A_SPEED = 10;
+const int DIRECTION_PIN = 7;
+#define NEGATIVE_DIR LOW
+#define POSITIVE_DIR HIGH
 
-// Motor B
-const int MOTOR_B_DIR_1 = 11;
-const int MOTOR_B_DIR_2 = 8;
-const int MOTOR_B_SPEED = 9;
+// Copied from one of the official examples:
+// For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
+// For MCP4725A0 the address is 0x60 or 0x61
+// For MCP4725A2 the address is 0x64 or 0x65
+const int MCP4725_ADDRESS = 0x62;
 
-const int output_pins[] = {
-    MOTOR_A_DIR_1,
-    MOTOR_A_DIR_2,
-    MOTOR_A_SPEED,
-    MOTOR_B_DIR_1,
-    MOTOR_B_DIR_2,
-    MOTOR_B_SPEED
-};
+// Max Value to Pass to the DAC
+const int MCP4725_MAX = 4095;
 
 // Other Constants ===========================================================
 
-const int RE_RANGE = 10;
-const int SPEED_MAX = 255;
+// If a button is being pressed, this is how much it will change between loop()
+// iterations as a percent
+const int ACCELERATE_STEP = 5;
+
+// If no button is pressed, this is how much it will change between loop()
+// iterations as a percent decelerate
+const int DECELERATE_STEP = 20;
+
+// Number of milliseconds to wait between loop() iterations
 const int TICK = 100;
 
 // Global Variables ==========================================================
 
-// Speed and direction of the motor
-int speed = 0;
+/*
+ * Current Speed of the Motor as a Percent
+ * Goes from -100 to 100
+ */
+int current_speed = 0;
 
-// Rotary Encoder State
-int re_count = 0;
-int re_last_clk = 0;
+/*
+ * Target Speed of the Motor as a Percent
+ * Goes from -100 to 100
+ */
+int target_speed = 0;
 
-#ifdef LOG
+/*
+ * DAC Driver Object
+ */
+Adafruit_MCP4725 dac1;
+
+#if LOG == 1
 // Switch Update States (For Debug Logging Only)
-bool on_printed = false
-bool off_printed = false;
+bool forward_printed = false;
+bool backward_printed = false;
 #endif
 
 // Functions =================================================================
 
-// Called when Rotary Encoder Changes
-void re_interrupt() {
-    int re_clk = digitalRead(RE_CLK);
-    int re_dt = digitalRead(RE_DT);
-    if (re_last_clk != re_clk) {
-        re_last_clk = re_clk;
-        int pos = re_count < RE_RANGE ? 1 : 0;
-        int neg = re_count > -RE_RANGE ? -1 : 0;
-        re_count += ((re_clk != re_dt) ? pos : neg);
-    }
-#ifdef LOG
-    // Print New Speed
-    Serial.print("SPEED: ");
-    if (re_count) {
-        Serial.print(re_count);
-    }
-    Serial.println("0%");
-#endif
-}
-
 void setup() {
-#ifdef LOG
+    dac1.begin(MCP4725_ADDRESS);
+
+    pinMode(DIRECTION_PIN, OUTPUT);
+
+#if LOG == 1
     // Set up Serial Output
     Serial.begin(9600);
 
     // Print Initial Speed
-    Serial.print("SPEED: ");
-    if (re_count) {
-        Serial.print(re_count);
-    }
-    Serial.println("0%");
+    print_speed();
 #endif
-
-    // Set Output Pins
-    int output_pin_count = sizeof(output_pins) / sizeof(output_pins[0]);
-    for (int i = 0; i < output_pin_count; i++) {
-        pinMode(output_pins[i], OUTPUT);
-    }
-
-    // Set up RE Interrupt
-    attachInterrupt(RE_INTERRUPT, re_interrupt, CHANGE);
 }
 
 void loop() {
-    // Update Speed
-    if (SWITCH_1_PRESSED) {
-        speed = SPEED_MAX * (((float) re_count) / RE_RANGE);
 
-        // Limit Speed Just In Case
-        if (speed > SPEED_MAX)
-            speed = SPEED_MAX;
-        else if (speed < -SPEED_MAX)
-            speed = -SPEED_MAX;
-
-#ifdef LOG
-        // Print ON if not done already
-        if (!on_printed) {
-            Serial.println("ON");
-            on_printed = true;
-            off_printed = false;
+    // Update Target Speed By Checking Switches
+    int speed_step;
+    bool foward = FOWARD_SWITCH_PRESSED;
+    bool backward = BACKWARD_SWITCH_PRESSED;
+    if (foward && !backward) {
+        target_speed = FORWARD_DIR(100);
+        speed_step = ACCELERATE_STEP;
+#if LOG == 1
+        // Print FORWARD if not done already
+        if (!foward_printed) {
+            Serial.println("FORWARD PRESSED");
+            forward_printed = true;
+            backward_printed = false;
+        }
+#endif
+    } else if (backward && !forward) {
+        target_speed = BACKWARD_DIR(100);
+        speed_step = ACCELERATE_STEP;
+#if LOG == 1
+        // Print FORWARD if not done already
+        if (!backward_printed) {
+            Serial.println("BACKWARD PRESSED");
+            forward_printed = false;
+            backward_printed = true;
         }
 #endif
     } else {
-        speed = 0;
-#ifdef LOG
-        // Print OFF if not done already
-        if (!off_printed) {
+        // Neither Button is being pressed or both are being pressed, so we shouldn't
+        // do anything.
+        target_speed = 0;
+        speed_step = DECELERATE_STEP;
+#if LOG == 1
+        // Print FORWARD if not done already
+        if (forward_printed || backward_printed) {
             Serial.println("OFF");
-            off_printed = true;
-            on_printed = false;
+            forward_printed = false;
+            backward_printed = false;
         }
 #endif
     }
 
-    // Update Motor
-    if (speed >= 0) {
-        digitalWrite(MOTOR_A_DIR_1, LOW);
-        digitalWrite(MOTOR_A_DIR_2, HIGH);
-        analogWrite(MOTOR_A_SPEED, speed);
-    } else {
-        digitalWrite(MOTOR_A_DIR_1, HIGH);
-        digitalWrite(MOTOR_A_DIR_2, LOW);
-        analogWrite(MOTOR_A_SPEED, -speed);
+    // Update Current Speed
+    // Move it closer to target speed if it's not the same.
+    if (target_speed != current_speed) {
+        if (target_speed > current_speed) {
+            current_speed += speed_step;
+            if (current_speed > target_speed)
+                current_speed = target_speed;
+        } else {
+            current_speed -= speed_step;
+            if (current_speed < target_speed)
+                current_speed = target_speed;
+        }
+        LOG_SPEED;
     }
+
+    // Update Output
+    int abs_speed = 0;
+    if (current_speed > 0) {
+        abs_speed = current_speed;
+        digitalWrite(DIRECTION_PIN, POSITIVE_DIR);
+    } else if (current_speed < 0) {
+        abs_speed = -current_speed;
+        digitalWrite(DIRECTION_PIN, NEGATIVE_DIR);
+    }
+    dac1.setVoltage(MPC4725_MAX * abs_speed / 100, false);
 
     // Sleep for a While
     delay(TICK);
