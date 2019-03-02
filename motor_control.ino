4ac67e6e98c8391212bec029f53d34441d856473
Fred Hornsey <fred@hornsey.us>
2019-03-01 23:43:33 -0600

    Got DAC Working
    - Removed LN Driver Code
    - Correct I2C Code for Sparkfun MPC
    - Added Ramp POT
    - Direction pin using I2C Relays
    - Start Prevent Opposite Direction Delay (TODO Still)

--- motor_control.ino
+++ motor_control.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+
 /*
  * Serial Logging Control
  * Should be disabled when this is uploaded for the last time
@@ -16,74 +18,57 @@
 #define LOG_SPEED // Nothing
 #endif
 
-/*
- * Set Motor Driver
- */
-#define L298N 0
-#define MCP4725 1
-#define DRIVER MCP4725
-// #define DRIVER L298N
-
-#if DRIVER == MCP4725
-#include <Wire.h>
-#include <Adafruit_MCP4725.h>
-#endif
-
 // Input =====================================================================
 
-#define FORWARD_SWITCH_PRESSED (digitalRead(8) == HIGH)
 #define FORWARD_DIR(value) (value)
-#define BACKWARD_SWITCH_PRESSED (digitalRead(9) == HIGH)
-#define BACKWARD_DIR(value) -(value)
+const int FORWARD_PIN = 4;
 
-#define POT1 analogRead(10)
-const int POT1_MAX = 1023;
+#define BACKWARD_DIR(value) -(value)
+const int BACKWARD_PIN = 5;
+
+const int POT_MAX = 1023;
+#define RAMP_POT analogRead(A0)
+#define MAX_SPEED_POT analogRead(A1)
 
 // Output ====================================================================
 
-#define NEGATIVE_DIR LOW
-#define POSITIVE_DIR HIGH
+// Output Pins ---------------------------------------------------------------
+
+const int ENABLE_PIN = A2;
+
+const int DIRECTION_PIN = A3;
+#define NEGATIVE_DIR false
+#define POSITIVE_DIR true
+
+const int output_pins[] = {
+    ENABLE_PIN,
+    DIRECTION_PIN
+};
 
 // MCP4725 -------------------------------------------------------------------
-#if DRIVER == MCP4725
-
-const int DIRECTION_PIN = 3;
-
-// Copied from one of the official examples:
-// For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
-// For MCP4725A0 the address is 0x60 or 0x61
-// For MCP4725A2 the address is 0x64 or 0x65
-const int MCP4725_ADDRESS = 0x62;
+const int MCP4725_ADDRESS = 0x60;
 
 // Max Value to Pass to the DAC
 const int MCP4725_MAX = 4095;
 
-// L298N ---------------------------------------------------------------------
-#else
+// Relays --------------------------------------------------------------------
+// From https://learn.sparkfun.com/tutorials/qwiic-quad-relay-hookup-guide
+const int RELAY_ADDRESS = 0x6D;
 
-const int DIR_1_PIN = 13;
-const int DIR_2_PIN = 12;
-
-const int SPEED_PIN = 10;
-const int L298N_MAX = 255;
-#endif
-
-// Output Pins ---------------------------------------------------------------
-const int output_pins[] = {
-#if DRIVER == MCP4725
-    DIRECTION_PIN
-#else // L298N
-    DIR_1_PIN,
-    DIR_2_PIN,
-    SPEED_PIN
-#endif
-};
+// I2C Commands
+const int TOGGLE_RELAY_ONE = 0x01;
+const int TOGGLE_RELAY_TWO = 0x02;
+const int TOGGLE_RELAY_THREE = 0x03;
+const int TOGGLE_RELAY_FOUR = 0x04;
+const int TURN_ALL_ON = 0xB;
+const int TURN_ALL_OFF = 0xA;
+const int TOGGLE_ALL = 0xC;
+const int RELAY_ONE_STATUS = 0x05;
+const int RELAY_TWO_STATUS = 0x06;
+const int RELAY_THREE_STATUS = 0x07;
+const int RELAY_FOUR_STATUS = 0x08;
 
 // Other Constants ===========================================================
-
-// If a button is being pressed, this is how much speed increase will change
-// between loop() iterations as a percent
-const int ACCELERATE_STEP = 3;
 
 /*
  * The common denominator for speed. 100 means the speed is a percentage of the
@@ -114,13 +99,6 @@ int current_speed = 0;
  */
 int target_speed = 0;
 
-#if DRIVER == MCP4725
-/*
- * DAC Driver Object
- */
-Adafruit_MCP4725 dac1;
-#endif
-
 // Variables Used For Debug Logging Only
 #if LOG == 1
 // Switch Update States
@@ -131,18 +109,84 @@ bool backward_printed = true;
 int last_max_speed = 0;
 #endif
 
+/*
+ * Disable opposite direction for 1 second after releasing current direction
+ * Motor (BLWS23MDA) is asking for this
+ * State Diagram looks like this:
+ *
+ *         +---------------(TIMEOUT)----------+  +----------------(TIMEOUT)-------+
+ *         |                                  V  V                                |
+ * FORWARD_DISABLED <-(RELEASED)- BACKWARD <- IDLE -> FORWARD -(RELEASED)> BACKWARD_DISABLED
+ *         |                          ^                  ^                        |
+ *         +--------------------------+                  +------------------------+
+ */
+const unsigned long OPPOSITE_DELAY_TICKS = 10;
+bool disable_opposite_dir = false;
+bool last_dir; // true is forward, false is backward
+bool timedout = false;
+
 // Functions =================================================================
 
-void setup() {
-#if DRIVER == MCP4725
-    dac1.begin(MCP4725_ADDRESS);
+void set_speed(int speed) {
+    int value = ((long) MCP4725_MAX) * speed / DENOMINATOR;
+
+    // Based on Sparkfun Example
+    Wire.beginTransmission(MCP4725_ADDRESS);
+    Wire.write(64);
+    Wire.write(value >> 4);
+    Wire.write((value & 15) << 4);
+#if LOG == 1
+    int result =
 #endif
+    Wire.endTransmission();
+#if LOG == 1
+    switch (result) {
+    case 0:
+        Serial.println("set_speed(): Successfully wrote speed");
+        break;
+    case 1:
+        Serial.println("set_speed(): ERROR: Can't send more than 32 bytes");
+        break;
+    case 2:
+        Serial.println("set_speed(): ERROR: NACK on address transmit");
+        break;
+    case 3:
+        Serial.println("set_speed(): ERROR: NACK on data transmit");
+        break;
+    default:
+        Serial.print("set_speed(): ERROR: unknown error code: ");
+        Serial.println(result);
+        break;
+    }
+#endif
+}
+
+void set_direction(bool positive) {
+    if (last_dir == positive) {
+        return;
+    } else {
+        last_dir = positive;
+    }
+    Wire.beginTransmission(RELAY_ADDRESS);
+    Wire.write(TURN_ALL_OFF);
+    Wire.endTransmission();
+    if (positive) {
+        Wire.beginTransmission(RELAY_ADDRESS);
+        Wire.write(TOGGLE_RELAY_ONE);
+        Wire.endTransmission();
+    }
+}
+
+void setup() {
+    Wire.begin();
 
     // Set Output Pins
     int output_pin_count = sizeof(output_pins) / sizeof(output_pins[0]);
     for (int i = 0; i < output_pin_count; i++) {
         pinMode(output_pins[i], OUTPUT);
     }
+
+    digitalWrite(ENABLE_PIN, LOW);
 
 #if LOG == 1
     // Set up Serial Output
@@ -151,12 +195,12 @@ void setup() {
     // Print Initial Speed
     LOG_SPEED;
 #endif
+
 }
 
 void loop() {
-
     // Update Max Speed
-    max_speed = ((long) POT1) * DENOMINATOR / POT1_MAX;
+    max_speed = ((long) MAX_SPEED_POT) * DENOMINATOR / POT_MAX;
 #if LOG == 1
     if (abs(max_speed - last_max_speed)) {
         Serial.print("MAX: ");
@@ -167,13 +211,21 @@ void loop() {
     }
 #endif
 
+    // Get Ramp
+    int ramp = ((long) RAMP_POT) * DENOMINATOR / (POT_MAX * 10);
+    //ramp = ((long) ramp) * ((long) max_speed) / DENOMINATOR;
+    if (ramp == 0) {
+        // Ramp should be at least 1
+        ramp = 1;
+    }
+
     // Update Target Speed By Checking Switches
-    int speed_step;
-    bool forward = FORWARD_SWITCH_PRESSED;
-    bool backward = BACKWARD_SWITCH_PRESSED;
+    bool forward = digitalRead(FORWARD_PIN) == HIGH;
+    bool backward = digitalRead(BACKWARD_PIN) == HIGH;
     if (forward && !backward) {
+        digitalWrite(ENABLE_PIN, HIGH);
         target_speed = FORWARD_DIR(max_speed);
-        speed_step = ACCELERATE_STEP;
+
 #if LOG == 1
         // Print FORWARD PRESSED if not done already
         if (!forward_printed) {
@@ -183,8 +235,9 @@ void loop() {
         }
 #endif
     } else if (backward && !forward) {
+        digitalWrite(ENABLE_PIN, HIGH);
         target_speed = BACKWARD_DIR(max_speed);
-        speed_step = ACCELERATE_STEP;
+
 #if LOG == 1
         // Print BACKWARD PRESSED if not done already
         if (!backward_printed) {
@@ -194,6 +247,8 @@ void loop() {
         }
 #endif
     } else {
+        digitalWrite(ENABLE_PIN, LOW);
+
         // Neither Button is being pressed or both are being pressed, so we shouldn't
         // do anything.
         target_speed = 0;
@@ -212,11 +267,11 @@ void loop() {
     // Move it closer to target speed if it's not the same.
     if (target_speed != current_speed) {
         if (target_speed > current_speed) {
-            current_speed += speed_step;
+            current_speed += ramp;
             if (current_speed > target_speed)
                 current_speed = target_speed;
         } else {
-            current_speed -= speed_step;
+            current_speed -= ramp;
             if (current_speed < target_speed)
                 current_speed = target_speed;
         }
@@ -227,28 +282,14 @@ void loop() {
     int abs_speed = 0;
     if (current_speed > 0) {
         abs_speed = current_speed;
-#if DRIVER == MCP4725
-        digitalWrite(DIRECTION_PIN, POSITIVE_DIR);
-#else // L298N
-        digitalWrite(DIR_1_PIN, POSITIVE_DIR);
-        digitalWrite(DIR_2_PIN, NEGATIVE_DIR);
-#endif
+        set_direction(POSITIVE_DIR);
     } else if (current_speed < 0) {
         abs_speed = -current_speed;
-#if DRIVER == MCP4725
-        digitalWrite(DIRECTION_PIN, NEGATIVE_DIR);
-#else // L298N
-        digitalWrite(DIR_1_PIN, NEGATIVE_DIR);
-        digitalWrite(DIR_2_PIN, POSITIVE_DIR);
-#endif
+        set_direction(NEGATIVE_DIR);
     }
 
     // Update Speed
-#if DRIVER == MCP4725
-    dac1.setVoltage(((long) MCP4725_MAX) * abs_speed / DENOMINATOR, false);
-#else // L298N
-    analogWrite(SPEED_PIN, ((long) L298N_MAX) * abs_speed / DENOMINATOR);
-#endif
+    set_speed(abs_speed);
 
     // Sleep for a While
     delay(TICK);
